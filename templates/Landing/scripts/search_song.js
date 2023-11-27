1a10e703c795b4983151ec6070011a0bc9489056
anitastuberg <anitaguld@hotmail.com>
2023-11-27 20:50:54 +0100

    first commit

---
+++ templates/Landing/scripts/search_song.js
@@ -0,0 +1,8 @@
+
+
+
+button = document.getElementById("submit_btn").onclick = myFunction();
+
+function myFunction() {
+    window.location.pathname = "file:///Users/anitastuberg/Library/Mobile%20Documents/com~apple~CloudDocs/Documents/Barcelona/ELISAVA/Self%20Data/Final%20project/templates/result.html";
+}
